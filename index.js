2146f135a9d79143102dfaba603983926b5387a0
Fer Hernandez <fh.1111@gmail.com>
2019-02-05 00:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key,value){
+function updateObjectWithKeyAndValue(object, key, value){
   return Object.assing({}, object, { [key]: value})
 }
