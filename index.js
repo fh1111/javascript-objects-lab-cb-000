0eb7899b8e6de13439e4d0faffae7c3da3e7a5e6
Fer Hernandez <fh.1111@gmail.com>
2019-02-05 00:48:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key]
   return newObj
 }
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
